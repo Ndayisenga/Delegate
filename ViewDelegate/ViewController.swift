a5f396ad82ed529f0da119ef666ec2d60f962343
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-04-24 10:58:53 +0200

    Initial Commit

---
+++ ViewDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewDelegate
+//
+//  Created by Ndayisenga Jean Claude on 24/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
